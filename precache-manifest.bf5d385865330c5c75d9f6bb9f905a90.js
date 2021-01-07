4cf0a389883f72c42d6cfc317c156f77de165de9
petermail <petermail@seznam.cz>
2021-01-07 17:31:24 +0100

    Updates

---
+++ precache-manifest.bf5d385865330c5c75d9f6bb9f905a90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8b7828f4bda59852b2ae1a6352f370",
+    "url": "/Presentation/index.html"
+  },
+  {
+    "revision": "2d4ead777f0f8b510b09",
+    "url": "/Presentation/static/css/main.b007f1e3.chunk.css"
+  },
+  {
+    "revision": "cffb5cba67506c15985e",
+    "url": "/Presentation/static/js/2.6354f821.chunk.js"
+  },
+  {
+    "revision": "42a31b586fadc7e83342bae79ad2096b",
+    "url": "/Presentation/static/js/2.6354f821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4ead777f0f8b510b09",
+    "url": "/Presentation/static/js/main.4dde49f0.chunk.js"
+  },
+  {
+    "revision": "943873b2cc7ac9796f31",
+    "url": "/Presentation/static/js/runtime-main.62e7db73.js"
+  }
+]);
